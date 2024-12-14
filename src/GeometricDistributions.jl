be759f62f884ac5150da6a95d23a780dbb8632b6
Joaquín Rodríguez <joaq.rod0@gmail.com>
2024-12-14 18:10:31 -0300

    add fallback to rand!

--- src/GeometricDistributions.jl
+++ src/GeometricDistributions.jl
@@ -10,6 +10,14 @@ Abstract type to represent distributions in geometric space.
 """
 abstract type GeometricDistribution end
 
+# Fallback to rand! when rand is not defined:
+function Random.rand(rng::AbstractRNG, d::Random.SamplerTrivial{<:GeometricDistribution})
+    v = similar(Random.eltype(d))
+    rand!(rng, v, d)
+    convert(T, v)
+end
+
+
 include("UniformDirection.jl")
 
 end
