6124f8db1b2b0444edbccce09f7cf9feab5045db
Joaquín Rodríguez <joaq.rod0@gmail.com>
2024-12-15 13:48:37 -0300

    Add sanity typechecks

--- src/UniformDirection.jl
+++ src/UniformDirection.jl
@@ -16,17 +16,19 @@ UniformDirection() = UniformDirection(Missing)
 Random.eltype(::Type{UniformDirection{T}}) where T = T
 
 function Random.rand(rng::AbstractRNG, ::Random.SamplerTrivial{UniformDirection{T}, T}) where T
+    # from https://math.stackexchange.com/a/1585996
     v = randn(rng, T)
     while iszero(v)
         v = randn(rng, T)
     end
-    normalize(v)
+    normalize(v) :: T
 end
 
 function Random.rand!(rng::AbstractRNG, v::AbstractArray{<:Number}, ::Random.SamplerTrivial{<:UniformDirection})
+    # from https://math.stackexchange.com/a/1585996
     randn!(rng, v)
     while iszero(v)
         randn!(rng, v)
     end
-    normalize!(v)
+    normalize!(v) :: typeof(v)
 end
